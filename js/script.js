aa7512ed95275046432dad4fd8d27d3f7cdfedfc
Chea <100029619+OuThorninvithyea@users.noreply.github.com>
2026-03-18 04:44:34 +0700

    Add like btn

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let activeDropdown = null;
+const likeBtn = document.querySelectorAll(".like-btn");
 
 document.querySelectorAll(".three-dot-post").forEach((dot) => {
   dot.addEventListener("click", (e) => {
@@ -19,4 +20,27 @@ document.addEventListener("click", () => {
     activeDropdown.classList.remove("active");
     activeDropdown = null;
   }
+});
+
+likeBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const textNode = btn.childNodes[btn.childNodes.length - 1];
+    let count = parseInt(textNode.textContent.trim()) || 0;
+
+    if (btn.classList.contains("liked")) {
+      count -= 1;
+      btn.classList.remove("liked");
+      btn.style.color = "";
+      btn.style.backgroundColor = "";
+      btn.style.border = "";
+    } else {
+      count += 1;
+      btn.classList.add("liked");
+      btn.style.color = "oklch(0.637 0.237 25.331)";
+      btn.style.backgroundColor = "oklch(0.936 0.032 17.717)";
+      btn.style.border = "1px solid oklch(0.637 0.237 25.331)";
+    }
+
+    textNode.textContent = " " + count;
+  });
 });
