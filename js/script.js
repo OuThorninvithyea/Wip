d5cc66b72ee755dc45629e14ad9e40e510e074ff
Chea <100029619+OuThorninvithyea@users.noreply.github.com>
2026-03-17 19:49:09 +0700

    Ther ban pint nis hx

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 const threeDotPost = document.querySelector(".three-dot-post");
+const threeDotClick = document.querySelector(".three-dot-click");
 
+if (threeDotPost && threeDotClick) {
+  threeDotPost.addEventListener("click", (e) => {
+    e.stopPropagation();
+    threeDotClick.classList.toggle("active");
+  });
+
+  document.addEventListener("click", () => {
+    threeDotClick.classList.remove("active");
+  });
+}
